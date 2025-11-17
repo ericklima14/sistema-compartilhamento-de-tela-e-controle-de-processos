a91f3f0c7b592271b1a9b4ac8c3f885fb7b1f801
Gustavo Silva <gustavosilvadeoliveira02@gmail.com>
2025-11-17 15:57:05 -0300

    ADD: Visualizar os processos do aluno no FormFoco (falta dar aquele TAPINHA maneiro na UI).

--- Professor/FormFoco.Designer.cs
+++ Professor/FormFoco.Designer.cs
@@ -28,35 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             videoView1 = new LibVLCSharp.WinForms.VideoView();
+            splitContainer1 = new SplitContainer();
+            btnMatarProcesso = new Button();
+            lvProcessos = new ListView();
+            headerPrograma = new ColumnHeader();
+            imageListProcessos = new ImageList(components);
+            lblProcessosAluno = new Label();
             ((System.ComponentModel.ISupportInitialize)videoView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
             SuspendLayout();
             // 
             // videoView1
             // 
-            videoView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             videoView1.BackColor = Color.Black;
-            videoView1.Location = new Point(12, 27);
+            videoView1.Dock = DockStyle.Fill;
+            videoView1.Location = new Point(0, 0);
             videoView1.MediaPlayer = null;
             videoView1.Name = "videoView1";
-            videoView1.Size = new Size(776, 411);
+            videoView1.Size = new Size(597, 450);
             videoView1.TabIndex = 0;
             videoView1.Text = "videoView1";
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.Location = new Point(0, 0);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(videoView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(btnMatarProcesso);
+            splitContainer1.Panel2.Controls.Add(lvProcessos);
+            splitContainer1.Panel2.Controls.Add(lblProcessosAluno);
+            splitContainer1.Size = new Size(800, 450);
+            splitContainer1.SplitterDistance = 597;
+            splitContainer1.TabIndex = 1;
+            // 
+            // btnMatarProcesso
+            // 
+            btnMatarProcesso.Location = new Point(-1, 409);
+            btnMatarProcesso.Name = "btnMatarProcesso";
+            btnMatarProcesso.Size = new Size(200, 41);
+            btnMatarProcesso.TabIndex = 14;
+            btnMatarProcesso.Text = "Matar Processo";
+            btnMatarProcesso.UseVisualStyleBackColor = true;
+            // 
+            // lvProcessos
+            // 
+            lvProcessos.CheckBoxes = true;
+            lvProcessos.Columns.AddRange(new ColumnHeader[] { headerPrograma });
+            lvProcessos.FullRowSelect = true;
+            lvProcessos.Location = new Point(-1, 18);
+            lvProcessos.Name = "lvProcessos";
+            lvProcessos.Size = new Size(200, 385);
+            lvProcessos.SmallImageList = imageListProcessos;
+            lvProcessos.TabIndex = 13;
+            lvProcessos.UseCompatibleStateImageBehavior = false;
+            lvProcessos.View = View.Details;
+            // 
+            // headerPrograma
+            // 
+            headerPrograma.Text = "Programa";
+            headerPrograma.Width = 250;
+            // 
+            // imageListProcessos
+            // 
+            imageListProcessos.ColorDepth = ColorDepth.Depth32Bit;
+            imageListProcessos.ImageSize = new Size(16, 16);
+            imageListProcessos.TransparentColor = Color.Transparent;
+            // 
+            // lblProcessosAluno
+            // 
+            lblProcessosAluno.AutoSize = true;
+            lblProcessosAluno.Location = new Point(-1, 0);
+            lblProcessosAluno.Name = "lblProcessosAluno";
+            lblProcessosAluno.Size = new Size(112, 15);
+            lblProcessosAluno.TabIndex = 12;
+            lblProcessosAluno.Text = "Processos do aluno:";
+            lblProcessosAluno.Visible = false;
             // 
             // FormFoco
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(videoView1);
+            Controls.Add(splitContainer1);
             Name = "FormFoco";
             Text = "FormFoco";
             ((System.ComponentModel.ISupportInitialize)videoView1).EndInit();
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel2.ResumeLayout(false);
+            splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
         private LibVLCSharp.WinForms.VideoView videoView1;
+        private SplitContainer splitContainer1;
+        private Button btnMatarProcesso;
+        private ListView lvProcessos;
+        private ColumnHeader headerPrograma;
+        private Label lblProcessosAluno;
+        private ImageList imageListProcessos;
     }
 }
--- Professor/FormFoco.cs
+++ Professor/FormFoco.cs
@@ -31,11 +31,14 @@ namespace Professor
             this.Port = port;
             this.NomeAluno = nomeAluno;
 
+            this.lblProcessosAluno.Text = $"Processos: {this.NomeAluno}";
+            this.btnMatarProcesso.Click += btnMatarProcesso_Click; // Conecta o evento
+
             this.Load += FormFoco_Load;
             this.FormClosing += FormFoco_FormClosing;
         }
 
-        private void FormFoco_Load(object sender, EventArgs e)
+        private async void FormFoco_Load(object sender, EventArgs e)
         {
             _mediaPlayer = new MediaPlayer(_libVLC);
             videoView1.MediaPlayer = _mediaPlayer;
@@ -60,12 +63,69 @@ namespace Professor
             {
                 MessageBox.Show($"Erro ao focar no stream: {ex.Message}");
             }
+
+            ProcessosManager.Instance.ListaProcessosAtualizada += AtualizarListaProcessos;
+            await Task.Run(async () => await ProcessosManager.Instance.IniciarMonitoramento(NomeAluno));
         }
 
         private void FormFoco_FormClosing(object sender, FormClosingEventArgs e)
         {
             _mediaPlayer?.Stop();
             _mediaPlayer?.Dispose();
+
+            ProcessosManager.Instance.ListaProcessosAtualizada -= AtualizarListaProcessos;
+            Task.Run(async () => await ProcessosManager.Instance.PararMonitoramento());
+        }
+
+        private async void btnMatarProcesso_Click(object sender, EventArgs e)
+        {
+            List<string> processos = lvProcessos.CheckedItems.Cast<ListViewItem>().Select(item => item.Text).ToList();
+            await ProcessosManager.Instance.MatarProcessos(processos);
+        }
+
+        private void AtualizarListaProcessos(string[] processos)
+        {
+            if (lvProcessos.InvokeRequired)
+            {
+                lvProcessos.Invoke(new Action(() => AtualizarListaProcessos(processos)));
+                return;
+            }
+
+            var checkedItems = lvProcessos.CheckedItems.Cast<ListViewItem>().Select(i => i.Text).ToHashSet();
+            int topIndex = lvProcessos.TopItem?.Index ?? 0;
+
+            lvProcessos.BeginUpdate();
+            lvProcessos.Items.Clear();
+            imageListProcessos.Images.Clear();
+            imageListProcessos.Images.Add(SystemIcons.Application);
+
+            var iconCache = new Dictionary<string, int>();
+            foreach (var proc in processos)
+            {
+                if (string.IsNullOrEmpty(proc)) continue;
+
+                int iconIndex = 0;
+                if (ProcessosManager.Instance.CaminhosDeIcone.TryGetValue(proc, out var caminho) && !string.IsNullOrEmpty(caminho))
+                {
+                    try
+                    {
+                        var icon = Icon.ExtractAssociatedIcon(caminho);
+                        if (icon != null)
+                        {
+                            imageListProcessos.Images.Add(icon);
+                            iconIndex = imageListProcessos.Images.Count - 1;
+                        }
+                    }
+                    catch { }
+                }
+
+                var item = new ListViewItem(proc, iconIndex) { Checked = checkedItems.Contains(proc) };
+                lvProcessos.Items.Add(item);
+            }
+
+            lvProcessos.EndUpdate();
+            if (lvProcessos.Items.Count > topIndex)
+                lvProcessos.EnsureVisible(topIndex);
         }
     }
 }
