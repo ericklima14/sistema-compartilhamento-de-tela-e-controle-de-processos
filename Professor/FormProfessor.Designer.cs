ccab9321cd16347a07898de3fefdb5258c00ac4a
Gustavo Silva <gustavosilvadeoliveira02@gmail.com>
2025-11-22 02:18:56 -0300

    professor BONITO!!!!

--- Professor/FormProfessor.Designer.cs
+++ Professor/FormProfessor.Designer.cs
@@ -29,35 +29,34 @@
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
-            label1 = new Label();
             lstAlunosConectados = new ListBox();
             imageListProcessos = new ImageList(components);
             btnGerenciarBloqueios = new Button();
             btnIniciarTelas = new Button();
             btnPararTelas = new Button();
             lblMonitorarTelas = new Label();
-            lblMensagens = new Label();
             lstLog = new ListBox();
             flpStudentStreams = new FlowLayoutPanel();
+            tlpPrincipal = new TableLayoutPanel();
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            groupBox3 = new GroupBox();
+            flpStudentStreams.SuspendLayout();
+            tlpPrincipal.SuspendLayout();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
             SuspendLayout();
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(1134, 26);
-            label1.Name = "label1";
-            label1.Size = new Size(110, 15);
-            label1.TabIndex = 5;
-            label1.Text = "Alunos Conectados";
             // 
             // lstAlunosConectados
             // 
+            lstAlunosConectados.Dock = DockStyle.Fill;
             lstAlunosConectados.FormattingEnabled = true;
             lstAlunosConectados.HorizontalScrollbar = true;
             lstAlunosConectados.ItemHeight = 15;
-            lstAlunosConectados.Location = new Point(1101, 59);
+            lstAlunosConectados.Location = new Point(3, 19);
             lstAlunosConectados.Name = "lstAlunosConectados";
-            lstAlunosConectados.Size = new Size(180, 154);
+            lstAlunosConectados.Size = new Size(238, 500);
             lstAlunosConectados.TabIndex = 6;
             lstAlunosConectados.DoubleClick += lstAlunosConectados_DoubleClick;
             // 
@@ -69,9 +68,11 @@
             // 
             // btnGerenciarBloqueios
             // 
-            btnGerenciarBloqueios.Location = new Point(851, 391);
+            btnGerenciarBloqueios.Dock = DockStyle.Top;
+            btnGerenciarBloqueios.Location = new Point(3, 91);
+            btnGerenciarBloqueios.Margin = new Padding(5);
             btnGerenciarBloqueios.Name = "btnGerenciarBloqueios";
-            btnGerenciarBloqueios.Size = new Size(172, 51);
+            btnGerenciarBloqueios.Size = new Size(238, 36);
             btnGerenciarBloqueios.TabIndex = 15;
             btnGerenciarBloqueios.Text = "Gerenciar Bloqueios";
             btnGerenciarBloqueios.UseVisualStyleBackColor = true;
@@ -79,9 +80,11 @@
             // 
             // btnIniciarTelas
             // 
-            btnIniciarTelas.Location = new Point(41, 50);
+            btnIniciarTelas.Dock = DockStyle.Top;
+            btnIniciarTelas.Location = new Point(3, 19);
+            btnIniciarTelas.Margin = new Padding(5);
             btnIniciarTelas.Name = "btnIniciarTelas";
-            btnIniciarTelas.Size = new Size(159, 36);
+            btnIniciarTelas.Size = new Size(238, 36);
             btnIniciarTelas.TabIndex = 16;
             btnIniciarTelas.Text = "Iniciar Monitoramento";
             btnIniciarTelas.UseVisualStyleBackColor = true;
@@ -89,9 +92,11 @@
             // 
             // btnPararTelas
             // 
-            btnPararTelas.Location = new Point(41, 92);
+            btnPararTelas.Dock = DockStyle.Top;
+            btnPararTelas.Location = new Point(3, 55);
+            btnPararTelas.Margin = new Padding(5);
             btnPararTelas.Name = "btnPararTelas";
-            btnPararTelas.Size = new Size(159, 36);
+            btnPararTelas.Size = new Size(238, 36);
             btnPararTelas.TabIndex = 17;
             btnPararTelas.Text = "Parar Monitoramento";
             btnPararTelas.UseVisualStyleBackColor = true;
@@ -100,64 +105,107 @@
             // lblMonitorarTelas
             // 
             lblMonitorarTelas.AutoSize = true;
-            lblMonitorarTelas.Location = new Point(41, 32);
+            lblMonitorarTelas.Location = new Point(13, 10);
             lblMonitorarTelas.Name = "lblMonitorarTelas";
             lblMonitorarTelas.Size = new Size(89, 15);
             lblMonitorarTelas.TabIndex = 19;
             lblMonitorarTelas.Text = "Monitorar Telas";
             // 
-            // lblMensagens
-            // 
-            lblMensagens.AutoSize = true;
-            lblMensagens.Location = new Point(877, 26);
-            lblMensagens.Name = "lblMensagens";
-            lblMensagens.Size = new Size(114, 15);
-            lblMensagens.TabIndex = 23;
-            lblMensagens.Text = "Caixa de Mensagens";
-            // 
             // lstLog
             // 
+            lstLog.Dock = DockStyle.Fill;
             lstLog.FormattingEnabled = true;
             lstLog.HorizontalScrollbar = true;
             lstLog.ItemHeight = 15;
-            lstLog.Location = new Point(792, 59);
+            lstLog.Location = new Point(3, 19);
             lstLog.Name = "lstLog";
             lstLog.RightToLeft = RightToLeft.No;
-            lstLog.Size = new Size(268, 154);
+            lstLog.Size = new Size(1045, 116);
             lstLog.TabIndex = 22;
             // 
             // flpStudentStreams
             // 
             flpStudentStreams.AutoScroll = true;
             flpStudentStreams.BorderStyle = BorderStyle.FixedSingle;
-            flpStudentStreams.Location = new Point(26, 154);
+            flpStudentStreams.Controls.Add(lblMonitorarTelas);
+            flpStudentStreams.Dock = DockStyle.Fill;
+            flpStudentStreams.Location = new Point(253, 147);
             flpStudentStreams.Name = "flpStudentStreams";
             flpStudentStreams.Padding = new Padding(10);
-            flpStudentStreams.Size = new Size(754, 493);
+            flpStudentStreams.Size = new Size(1051, 522);
             flpStudentStreams.TabIndex = 24;
+            // 
+            // tlpPrincipal
+            // 
+            tlpPrincipal.ColumnCount = 2;
+            tlpPrincipal.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 250F));
+            tlpPrincipal.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tlpPrincipal.Controls.Add(groupBox1, 0, 0);
+            tlpPrincipal.Controls.Add(groupBox2, 0, 1);
+            tlpPrincipal.Controls.Add(flpStudentStreams, 1, 1);
+            tlpPrincipal.Controls.Add(groupBox3, 1, 0);
+            tlpPrincipal.Dock = DockStyle.Fill;
+            tlpPrincipal.Location = new Point(0, 0);
+            tlpPrincipal.Name = "tlpPrincipal";
+            tlpPrincipal.RowCount = 2;
+            tlpPrincipal.RowStyles.Add(new RowStyle());
+            tlpPrincipal.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tlpPrincipal.Size = new Size(1307, 672);
+            tlpPrincipal.TabIndex = 25;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(btnGerenciarBloqueios);
+            groupBox1.Controls.Add(btnPararTelas);
+            groupBox1.Controls.Add(btnIniciarTelas);
+            groupBox1.Dock = DockStyle.Top;
+            groupBox1.Location = new Point(3, 3);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(244, 138);
+            groupBox1.TabIndex = 25;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Controles da Aula";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(lstAlunosConectados);
+            groupBox2.Dock = DockStyle.Fill;
+            groupBox2.Location = new Point(3, 147);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(244, 522);
+            groupBox2.TabIndex = 26;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Alunos Online";
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(lstLog);
+            groupBox3.Dock = DockStyle.Fill;
+            groupBox3.Location = new Point(253, 3);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(1051, 138);
+            groupBox3.TabIndex = 27;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Log do Sistema";
             // 
             // FormProfessor
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1307, 672);
-            Controls.Add(flpStudentStreams);
-            Controls.Add(lblMensagens);
-            Controls.Add(lstLog);
-            Controls.Add(lblMonitorarTelas);
-            Controls.Add(btnPararTelas);
-            Controls.Add(btnIniciarTelas);
-            Controls.Add(btnGerenciarBloqueios);
-            Controls.Add(lstAlunosConectados);
-            Controls.Add(label1);
+            Controls.Add(tlpPrincipal);
             Name = "FormProfessor";
             Text = "Form1";
+            flpStudentStreams.ResumeLayout(false);
+            flpStudentStreams.PerformLayout();
+            tlpPrincipal.ResumeLayout(false);
+            groupBox1.ResumeLayout(false);
+            groupBox2.ResumeLayout(false);
+            groupBox3.ResumeLayout(false);
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
-        private Label label1;
         private ListBox lstAlunosConectados;
         private ImageList imageListProcessos;
         private ImageList imageListProcessosBloq;
@@ -165,8 +213,11 @@
         private Button btnIniciarTelas;
         private Button btnPararTelas;
         private Label lblMonitorarTelas;
-        private Label lblMensagens;
         private ListBox lstLog;
         private FlowLayoutPanel flpStudentStreams;
+        private TableLayoutPanel tlpPrincipal;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private GroupBox groupBox3;
     }
 }
